b6a789296bdab1a891b939697d5bed1e06d8ad49
zilayo <84344709+zilayo@users.noreply.github.com>
2024-02-29 16:14:38 +0000

    initial attempt

---
+++ geth-entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -eu
+
+VERBOSITY=3
+GETH_DATA_DIR=/blast/data
+GETH_CHAINDATA_DIR="{$GETH_DATA_DIR}/geth/chaindata"
+GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
+ADDITIONAL_ARGS=""
+
+mkdir -p $GETH_DATA_DIR
+
+sh -c "[ ! -f /blast/jwt.txt ] && openssl rand -hex 32 | tr -d '\n' > /blast/jwt.txt"
+
+if [ "${OP_GETH_ETH_STATS+x}" = x ]; then
+    ADDITIONAL_ARGS="$ADDITIONAL_ARGS --ethstats=$OP_GETH_ETH_STATS"
+fi
+
+if [ "${OP_GETH_BOOTNODES+x}" = x ]; then
+    ADDITIONAL_ARGS="$ADDITIONAL_ARGS --bootnodes=$OP_GETH_BOOTNODES"
+fi
+
+if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
+	echo "$GETH_CHAINDATA_DIR missing, running init"
+	echo "Initializing genesis."
+	geth --verbosity="$VERBOSITY" init \
+		--datadir="$GETH_DATA_DIR" \
+		"$GENESIS_FILE_PATH"
+else
+	echo "$GETH_CHAINDATA_DIR exists."
+fi
+
+exec geth \
+    --datadir="$GETH_DATA_DIR" \
+    --networkid=$CHAIN_ID \
+    --verbosity="$VERBOSITY" \
+    --http \
+    --http.corsdomain="*" \
+    --http.vhosts="*" \
+    --http.addr=0.0.0.0 \
+    --http.port="8545" \
+    --http.api=web3,debug,eth,net,engine \
+    --authrpc.addr=0.0.0.0 \
+    --authrpc.port="8551" \
+    --authrpc.vhosts="*" \
+    --authrpc.jwtsecret="/blast/jwt.txt" \
+    --ws \
+    --ws.addr=0.0.0.0 \
+    --ws.port="8546" \
+    --ws.origins="*" \
+    --ws.api=debug,eth,net,engine \
+    --metrics \
+    --metrics.addr=0.0.0.0 \
+    --metrics.port="6060" \
+    --syncmode="snap" \
+    --state.scheme="path" \
+    --maxpeers=100 \
+    --rollup.disabletxpoolgossip=true \
+    --rollup.sequencerhttp="https://sequencer.blast.io" \
+    --port="30303" \
+    $ADDITIONAL_ARGS
---
+++ op-node-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eu
+
+L1_RPC_URL="http://host.docker.internal:8545"
+L2_ENGINE_URL="http://blast-geth:8551"
+CHAIN_ID=81457
+
+until [ "$(curl -s -w '%{http_code}' -o /dev/null ${L2_ENGINE_URL})" -eq 401 ]; do
+  echo "waiting for geth to be ready"
+  sleep 5
+done
+
+PUBLIC_IP=$(curl -s v4.ident.me)
+
+exec op-node \
+  --l2.jwt-secret="/blast/jwt.txt" \
+  --l1="${L1_RPC_URL}" \
+  --l1.trustrpc=true \
+  --l2="${L2_ENGINE_URL}" \
+  --metrics.enabled=true \
+  --metrics.addr="0.0.0.0" \
+  --metrics.port=7300 \
+  --p2p.advertise.ip=${PUBLIC_IP} \
+  --p2p.listen.ip="0.0.0.0" \
+  --p2p.listen.tcp="9222" \
+  --p2p.listen.udp="9222" \
+  --snapshotlog.file="/tmp/op-node-snapshot-log" \
+  --rollup.config="/rollup.json" \
+  --p2p.bootnodes=enr:"-J-4QM3GLUFfKMSJQuP1UvuKQe8DyovE7Eaiit0l6By4zjTodkR4V8NWXJxNmlg8t8rP-Q-wp3jVmeAOml8cjMj__ROGAYznzb_HgmlkgnY0gmlwhA-cZ_eHb3BzdGFja4X947FQAIlzZWNwMjU2azGhAiuDqvB-AsVSRmnnWr6OHfjgY8YfNclFy9p02flKzXnOg3RjcIJ2YYN1ZHCCdmE,enr:-J-4QDCVpByqQ8nFqCS9aHicqwUfXgzFDslvpEyYz19lvkHLIdtcIGp2d4q5dxHdjRNTO6HXCsnIKxUeuZSPcEbyVQCGAYznzz0RgmlkgnY0gmlwhANiQfuHb3BzdGFja4X947FQAIlzZWNwMjU2azGhAy3AtF2Jh_aPdOohg506Hjmtx-fQ1AKmu71C7PfkWAw9g3RjcIJ2YYN1ZHCCdmE"
+

3b4b79a04763ba404d08876b903b4bda038d40d4
zilayo <84344709+zilayo@users.noreply.github.com>
2024-03-27 21:21:44 +0000

    fix: fix default genesis file path

--- geth-entrypoint.sh
+++ geth-entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 VERBOSITY=3
 GETH_DATA_DIR=/base/data
 GETH_CHAINDATA_DIR="${GETH_DATA_DIR}/geth/chaindata"
-GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
+GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-genesis.json}"
 
 mkdir -p $GETH_DATA_DIR
 
